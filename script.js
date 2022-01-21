4328371e9b451eb33e540518a3733bedb176dd99
Romka2005 <svetanok12@gmail.com>
2022-01-22 01:41:31 +0200

    резюме

--- script.js
+++ script.js
@@ -10,6 +10,37 @@ let signup2 = document.querySelector(".signup2");
 let back1 = document.querySelector(".back1");
 let back2 = document.querySelector(".back2");
 
+let tl = gsap.timeline();
+
+tl.from('.nav', {opacity:0, display: "none", height: 0,opacity:1, duration:1});
+
+
+let info2 = function(){
+    tl.to('.information', {display:"block"});
+    tl.to('.information', {width: 530, height: 300, padding: 15, opacity:1, duration:.1});
+
+    tl.from('.information h2', {opacity:0, x:-50, duration:.5});
+    tl.from('.information h1', {opacity:0, x:-50, duration:.5});
+    tl.from('.information h3', {opacity:0, y:-50, duration:.5});
+}
+let infooff = function(){
+    tl.to('.information', {opacity:1, width:0, height:0, duration:.3, opacity: 0});
+    tl.to('.information', {display:"none", duration: 1});
+}
+
+let registr2 = function(){
+    tl.to('.registration', {display:"block"});
+    tl.to('.registration', {width: 530, height: 560, padding: 15, opacity:1, duration:.1});
+    tl.from('.registration h2', {opacity:0, x:-400, duration:3});
+}
+let registroff = function(){
+    tl.to('.registration h2', {opacity:0, x:-400, duration:.8});
+    tl.to('.registration', {opacity:1, width:0, height:0, duration:.1, opacity: 0});
+    tl.to('.registration', {display:"none", duration: 1.6});
+    
+}
+
+
 
 back1.addEventListener("click", function(){
     switch(registr.style.background){
@@ -37,22 +68,18 @@ back2.addEventListener("click", function(){
 
 //переход между регистр. и входом
 signup1.addEventListener("click", function() {
-    console.log("ok");
     sign.style.display = "none";
     registr.style.display = "block";
 });
 signin1.addEventListener("click", function() {
-    console.log("ok");
     sign.style.display = "block";
     registr.style.display = "none";
 });
 signup2.addEventListener("click", function() {
-    console.log("ok");
     sign.style.display = "none";
     registr.style.display = "block";
 });
 signin2.addEventListener("click", function() {
-    console.log("ok");
     sign.style.display = "block";
     registr.style.display = "none";
 });
@@ -61,28 +88,30 @@ signin2.addEventListener("click", function() {
 //навигации инфо и формы
 info.addEventListener("click", function() {
     if(informat.style.display == "block"){
-        informat.style.display  = "none";
+        infooff();
     }
     else if(informat.style.display == "none" || registr.style.display == "block" || sign.style.display == "block"){
-        registr.style.display = "none";
         sign.style.display = "none";
-        informat.style.display = "block";
+        registroff();
+        info2();
     }
     else{
-        informat.style.display = "block";
+        info2();
     }
+    
 });
 form.addEventListener("click", function() {
     
     if(registr.style.display == "block"){
-        registr.style.display  = "none";
+        registroff();
     }
     else if(registr.style.display == "none" || informat.style.display == "block" || sign.style.display == "block"){
         sign.style.display = "none";
-        informat.style.display = "none";
-        registr.style.display = "block";
+        infooff();
+        registr2();
     }
     else{
-        registr.style.display = "block";
+        registr2();
     }
+    
 })
